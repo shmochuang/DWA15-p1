cdc4cd24ab5544961d5f0c62e6a40332424e2d5c
Shmo Chuang <elisajchuang@gmail.com>
2014-06-30 13:42:09 -0400

    committing index.php to jumpstart

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Elisa "Shmo" Chuang</title>
+    
+    <link rel= "icon" type = "image/ico" href="images/favicon.ico"> <!-- favicon for site -->
+    <link rel= "stylesheet" type = "text/css" href="css/blueprint/screen.css" media="screen, projection"> <!-- link to blueprint/screen.css -->
+    <link rel= "stylesheet" type = "text/css" href="css/blueprint/print.css" media="print"> <!-- link to blueprint/print.css -->
+    <link rel= "stylesheet" type="text/css" href="css/style.css"> <!-- link to custom stylesheet -->
+</head>
+<body>
+    <div class="span-24">
+      <h1>SHMO</h1>
+    </div>
+    <div class="span-12">.
+      <img class="span-12" src="images/me.jpg" alt="Me at Disney World.">
+    </div>
+    <div class="span-12 last">
+        <h2> In 2+ sentences describe who you are and what you do.</h2>
+          <p>I'm a live-in nanny at the moment and I'm in the ALM program for Digital Media and Instructional Design. Also, no one ever calls me "Elisa" - everyone calls me "Shmo".</p>
+      <h2>Why are you taking this course?</h2>
+        <p>I'd like to expand my knowledge of websites and development.</p>
+      <h2>What previous experience do you have with HTML/CSS?</h2>
+        <p>I've taken two web basics courses at Harvard already which covered HTML/CSS (DGMD E-20 and CSCI E-12).</p>
+      <h2>What previous experience do you have with programming?</h2>
+        <p>I've taken two programming courses at Harvard - one in Java (CSCI S-111) and one in C (CSCI E-50).</p>
+      <h2>Have you worked with PHP before? If so, to what extent?</h2>
+        <p>Only a little bit in the CS50 and E-12 courses.</p>
+      <h2>Are you working  on a Mac, PC, or some other platform?</h2>
+        <p>Mac</p>
+      <h2>Anything else we should know?</h2>
+        <p>I am SUPER EXCITED to take this course!</p>
+    </div>
+</body>
+</html>
